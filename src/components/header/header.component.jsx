bb2b47651c3c647d8c0fdb0a6324017514440859
DwanW <kunvang@yahoo.com>
2020-03-22 17:23:18 -0600

    added dropdown and action reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import {auth} from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
-            <Logo className='logo'/>
+            <Logo className='logo' />
         </Link>
         <div className='options'>
             <Link className="option" to='/shop'>
@@ -21,16 +23,21 @@ const Header = ({currentUser}) => (
             </Link>
             {
                 currentUser ?
-                <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const CartActionTypes = {
+    TOGGLE_CART_HIDDEN: 'TOGGEL_CART_HIDDEN'
+}
+
+export default CartActionTypes;
