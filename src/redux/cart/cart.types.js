0e4ace27597b72d6ba60eda74c3ef7827ce5c886
DwanW <kunvang@yahoo.com>
2020-03-28 16:42:12 -0600

    checkout page functionality done

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGEL_CART_HIDDEN',
     ADD_ITEM: 'Add_ITEM',
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
+    REMOVE_ITEM: 'REMOVE_ITEM'
 }
 
 export default CartActionTypes;
