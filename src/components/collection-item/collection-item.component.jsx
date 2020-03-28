b551880e5fabbef892b683afb9f834d2dd91d358
DwanW <kunvang@yahoo.com>
2020-03-28 10:53:08 -0600

    new action to add item to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-        <div
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`,
-            }} />
-        <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-        </div>
-    </div>
-);
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
-export default CollectionItem;
+const CollectionItem = ({ item, addItem }) => {
+    const { imageUrl, name, price } = item;
+
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }} />
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted>
+                Add To Cart
+         </CustomButton>
+        </div>
+    )
+};
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGEL_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGEL_CART_HIDDEN',
+    ADD_ITEM: 'Add_ITEM',
 }
 
 export default CartActionTypes;
