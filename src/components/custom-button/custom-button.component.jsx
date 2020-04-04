99d5e3ec986357d988e1a5c420882dc60bb5acb6
DwanW <kunvang@yahoo.com>
2020-04-04 15:12:13 -0600

    fixing styling error

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
-    <button className={`${inverted ? 'inverted':''} custom-button`} {...otherProps}>
+    <button className={`${inverted ? 'inverted':''} ${
+        isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
