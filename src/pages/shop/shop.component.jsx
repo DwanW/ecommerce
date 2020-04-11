7b142509c9539c5cab4728b2367ee83213e88a4f
DwanW <kunvang@yahoo.com>
2020-04-11 15:26:02 -0600

    added container pattern to refactor code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,50 +2,55 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-import { updateCollections } from '../../redux/shop/shop.actions';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
+import { fetchCollectionStartAsync } from '../../redux/shop/shop.actions';
 
-import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    };
-
-    unsubscribeFromSnapshot = null;
 
     componentDidMount() {
-        const { updateCollections } = this.props
-        const collectionRef = firestore.collection('collections');
+        const { fetchCollectionStartAsync } = this.props;
+        fetchCollectionStartAsync()
+        // const { updateCollections } = this.props
+        // const collectionRef = firestore.collection('collections');
+        
+        // ## doable but need navigating function to navigate the nested obj
+        // fetch('https://firestore.googleapis.com/v1/projects/ecommerce-db-bac64/databases/(default)/documents/collections')
+        // .then(response => response.json())
+        // .then(collections => console.log(collections))
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState({ loading: false });
-        });
+
+        // ### promise && then pattern (only get data when re-mount this component)
+        // collectionRef.get().then(
+        //     snapshot => {
+        //         const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+        //         updateCollections(collectionsMap);
+        //         this.setState({ loading: false });
+        //     }
+        // )
+        
+        // ### observer && observable pattern (get data whenever snapshot changes)
+        // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        //     const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+        //     updateCollections(collectionsMap);
+        //     this.setState({ loading: false });
+        // });
     }
 
     render() {
-        const { loading } = this.state;
         const { match } = this.props
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
             </div>
         );
     }
 }
 
 const mapDispatchtoProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+    fetchCollectionStartAsync: () => dispatch(fetchCollectionStartAsync())
 })
-
-
 
 export default connect(null, mapDispatchtoProps)(ShopPage);
